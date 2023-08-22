c205353f929502eb8cf6992b82523fa49c55a824
Siddharth <st22041@ormiston.school.nz>
2023-08-22 14:17:56 +1200

    Finally got those fancybox galleries to work

---
+++ src/fancyboxConfig.js
@@ -0,0 +1,12 @@
+Fancybox.bind('[data-fancybox]', {
+    Thumbs: {
+        type: "classic"
+    },
+    Toolbar: {
+        display: {
+            left: ["infobar"],
+            middle: [],
+            right: ["close"],
+        }
+    }
+  });
